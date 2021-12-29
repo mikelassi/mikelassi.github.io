4f15e7def097616741ea123be66150bbccfcdd6c
Michael <lassimichael@gmail.com>
2021-12-29 10:03:08 +0100

    Playing with wordpress

--- wp-login.php
+++ wp-login.php
@@ -2,7 +2,7 @@
 <html lang="en-US">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Log In &lsaquo; Mike&#039;s Personal Page &#8212; WordPress</title>
+	<title>Log In &lsaquo; Michael Lassi &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
 <link rel="stylesheet" id="dashicons-css" href="/wp-includes/css/dashicons.min.css?ver=5.8.2" type="text/css" media="all">
@@ -51,7 +51,7 @@
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="/">&larr; Go to Mike&#039;s Personal Page</a>		</p>
+			<a href="/">&larr; Go to Michael Lassi</a>		</p>
 			</div>
 	<script type="text/javascript" src="/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
 <script type="text/javascript" src="/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
@@ -90,7 +90,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="/wp-includes/js/wp-util.min.js?ver=5.8.2" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"03c036b256"};
+var userProfileL10n = {"user_id":"0","nonce":"21551b0f0c"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
